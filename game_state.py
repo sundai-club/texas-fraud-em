38198ca2057a190dd861e3efd5b12a55aea17836
igors <igor.sadalski@gmail.com>
2025-03-09 14:49:16 -0400

    crate functions to start the game

--- game_state.py
+++ game_state.py
@@ -2,6 +2,8 @@
 
 from dataclasses import dataclass
 from enum import Enum
+import random
+from collections import deque
 
 class CardValue(Enum):
     TWO = 2
@@ -42,7 +44,7 @@ class Action():
 class Player:
     cards: list[Card]
     action: Action
-    monies: int
+    money: int
     id: str
     thoughts: str
 
@@ -66,28 +68,49 @@ class GameState:
     chat: Chat
     pot: int
     action_history: ActionHistory
-    deck: list[Card]
-    num: int = 5
+    deck: deque[Card]
+
+    def create_deck(self) -> 'GameState':
+        self.deck = deque()
+        for suit in CardSuit:
+            for value in CardValue:
+                self.deck.append(Card(value, suit))
+        
+        deck_list = list(self.deck)
+        random.shuffle(deck_list)
+        self.deck = deque(deck_list)
+        return self
 
     def create_players(self) -> 'GameState':
-        pass
+        for i in range(self.num_players):
+            self.players[f"player_{i}"] = Player(cards=[], 
+                                                 action=Action(fold=False, increase=0), 
+                                                 money=1000, 
+                                                 id=f"player_{i}", 
+                                                 thoughts="")
+        return self
 
     def start_game(self) -> 'GameState':
-        self.shuffle_cards()
+        self.create_deck()
         self.deal_cards()
         return self
+
+    def play_game(self) -> 'GameState':
+        pass
+
+    def play_round(self) -> 'GameState':
+        pass
+
+    def play_step(self) -> 'GameState':
+        pass
 
     def set_cards(self) -> 'GameState':
         pass
 
-    def deal_cards(self) -> 'GameState':
-        pass
+    def deal_cards(self) -> None:
+        for player in self.players.values():
+            player.cards = [self.deck.popleft() for _ in range(2)]
 
-    def shuffle_cards(self) -> 'GameState':
-        pass
-
-    def simulation_step(self) -> 'GameState':
-        pass
 
     def query_llm(self) -> 'LLMResponse':
         pass
@@ -101,3 +124,8 @@ class LLMResponse:
     response: str
 
 
+if __name__ == "__main__":
+    game = GameState()
+    game.create_players()
+    game.start_game()
+    print(game)
